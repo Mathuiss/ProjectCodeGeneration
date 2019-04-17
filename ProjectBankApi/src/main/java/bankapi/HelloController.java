a1043559c5a825d6e70c57807016eaaa64a01448
Mathuis G <mathijsgeerlings@gmail.com>
2019-04-17 13:17:15 +0200

    Testing shit. no biggie

--- ProjectBankApi/src/main/java/bankapi/HelloController.java
+++ ProjectBankApi/src/main/java/bankapi/HelloController.java
@@ -13,11 +13,6 @@ public class HelloController {
     @Autowired
     NamedParameterJdbcTemplate jdbcTemplate;
 
-    @RequestMapping("/")
-    String hello() {
-        return "Hello World!";
-    }
-
     @Data
     static class Result {
         private final int left;
@@ -43,12 +38,15 @@ public class HelloController {
         }
     }
 
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World!";
+    }
+
     // SQL sample
     @RequestMapping("calc")
     Result calc(@RequestParam int left, @RequestParam int right) {
-        MapSqlParameterSource source = new MapSqlParameterSource()
-                .addValue("left", left)
-                .addValue("right", right);
+        MapSqlParameterSource source = new MapSqlParameterSource().addValue("left", left).addValue("right", right);
         return jdbcTemplate.queryForObject("SELECT :left + :right AS answer", source,
                 (rs, rowNum) -> new Result(left, right, rs.getLong("answer")));
     }
