2a9b1d0718ddc75f72a7e47ebedc227ec394e8f6
Unknown <mathijsgeerlings@gmail.com>
2019-06-06 13:53:40 +0200

    Transfer java beans error

--- nfbc/src/main/java/io/swagger/services/TransactionService.java
+++ nfbc/src/main/java/io/swagger/services/TransactionService.java
@@ -1,17 +1,20 @@
 package io.swagger.services;
 
+import io.swagger.repositories.TestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import io.swagger.model.Transaction;
-import io.swagger.model.TransactionRepository;
+import io.swagger.repositories.TransactionRepository;
 
 @Service
 public class TransactionService {
     private TransactionRepository transactions;
+    private TestRepository test;
 
-    public TransactionService(TransactionRepository transactions) {
-        this.transactions = transactions;
+    public TransactionService(TestRepository test) {
+        this.test = test;
     }
 
     // Creates a new transaction
