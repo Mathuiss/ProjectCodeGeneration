bd17fd6f0fa556a3f23ccc515cd757a3c66baa2a
Unknown <robbin.kragtwijk@gmail.com>
2019-06-10 11:14:13 +0200

    service begin code

--- nfbc/src/main/java/io/swagger/services/SessionService.java
+++ nfbc/src/main/java/io/swagger/services/SessionService.java
@@ -1,21 +1,64 @@
 package io.swagger.services;
 
 import io.swagger.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import io.swagger.repositories.SessionRepositories;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 @Service
 public class SessionService {
-    //repository aanmaken
+    @Autowired
     private SessionRepositories sessionRepos;
 
     private Collection<User> GetAllUsers(){
         return sessionRepos.getAllUsers();
     }
+    private ArrayList<User> getUserLijst(){
+        //ArrayList<User> userLijst = new ArrayList<>(GetAllUsers());
+        return new ArrayList<>(GetAllUsers());
+    }
 
-    public boolean userExist(int id){
+    public boolean userExist(String email){
+
+        for (User user : getUserLijst()) {
+            if (user.getEmail().equals(email)){
+                return true;
+            }
+        }
+        return  false;
+    }
+
+    public int getUserIdByEmail(String email){
+        for (User user : getUserLijst()) {
+            if (user.getEmail().equals(email)){
+                return user.getId();
+            }
+        }
+        return 0;
+    }
+
+    public boolean passwordCheck(int id, String password){
+        for (User user : getUserLijst()) {
+            if (user.getId().equals(id)){
+                if(user.getHash().equals(password)){
+                    return true;
+                }
+            }
+        }
+        return  false;
+    }
+
+    public boolean isEmployee(int id){
+        for (User user : getUserLijst()) {
+            if (user.getId().equals(id)){
+                if(user.isIsEmployee()){
+                    return true;
+                }
+            }
+        }
         return  false;
     }
 }
